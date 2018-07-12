0d84f5a6a81c0841c4150080de48782e4e24139a
Thiago Dutra <nullelement@users.noreply.github.com>
2018-07-12 19:05:34 -0300

    Update setup.py

--- pyglpi/setup.py
+++ pyglpi/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name = 'GLPI',
     packages = find_packages(),
-    version = '0.0.1',
+    version = '0.0.2',
     description = 'Python API for Calling GLPI Webservices',
     author='Clint Grimsley',
     author_email='dunno',
